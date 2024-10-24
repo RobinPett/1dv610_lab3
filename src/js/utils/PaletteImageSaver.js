bc4ee2bafb5ba2503e3d713d0af210e24c95fea1
rp222nc <rp222nc@student.lnu.se>
2024-10-24 23:40:56 +0200

    Abstract html2canvas

---
+++ src/js/utils/PaletteImageSaver.js
@@ -0,0 +1,33 @@
+
+/**
+ * Handles saving palettes as an image.
+ */
+
+import html2canvas from 'html2canvas'
+
+class PaletteImageSaver {
+
+    /**
+     * Save HTML element with palette to an image.
+     *
+     * @param {String} name 
+     * @param {HTMLDivElement} paletteElement 
+     */
+    async saveImage(name, paletteElement) {
+        try {
+            const paletteToCanvas = await html2canvas(paletteElement)
+
+            const image = new Image()
+            image.src = paletteToCanvas.toDataURL()
+    
+            const link = document.createElement('a')
+            link.href = image.src
+            link.download = `${name}.png`
+            link.click()
+        } catch (error) {
+            throw new Error('Failed to save image: ' + error.message)
+        }
+    }
+}
+
+export default PaletteImageSaver
